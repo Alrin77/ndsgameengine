417edbd83b58427825f400e2f506aabb75625211
bbr2832@gmail.com <bbr2832@gmail.com@21144042-5242-e64c-d35b-eb64d47aa59c>
2010-10-17 23:39:38 +0000

    Changed how the GameStateManager handles game states.  All game states are now included in GameStateManager.h. Game states are referenced by integer values. #define STATE_INTRO 0 #define STATE_OPTIONS 1 #define STATE_SETUP 2 #define STATE_PLAY 3 #define STATE_CREDIT 4 #define STATE_PAUSE 5

--- include/IGameStateManager.h
+++ include/IGameStateManager.h
@@ -6,8 +6,8 @@ class GameState;
 
 class IGameStateManager : protected IGameManager{
 public:
-	virtual void PushState(GameState* newState)=0;
-	virtual void ChangeState(GameState* newState, bool clearStack)=0;
+	virtual void PushState(int StateToPush)=0;
+	virtual void ChangeState(int newState, bool clearStack)=0;
 	virtual bool PopState()=0;
 	virtual GameState* TopState()=0;
 };
