1f755676a480ff97a405cd49fdd023d1c6c4966d
bbr2832@gmail.com <bbr2832@gmail.com@21144042-5242-e64c-d35b-eb64d47aa59c>
2010-10-17 23:49:54 +0000

    Adding GameStatePause.h/.cpp

---
+++ source/GameStatePause.cpp
@@ -0,0 +1,26 @@
+#include "GameStatePause.h"
+
+GameStatePause::GameStatePause(){
+}
+
+GameStatePause::~GameStatePause(){
+}
+
+void GameStatePause::Initialize(){
+}
+
+void GameStatePause::Update(){
+}
+
+void GameStatePause::Draw(){
+}
+
+void GameStatePause::Cleanup(){
+}
+
+void GameStatePause::Pause(){
+}
+
+void GameStatePause::Resume(){
+}
+
