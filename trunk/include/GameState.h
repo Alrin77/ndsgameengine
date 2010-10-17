d22a4a3eea5ede9c6b7c8ff8e9086910d1c2b1a2
bmp410@gmail.com <bmp410@gmail.com@21144042-5242-e64c-d35b-eb64d47aa59c>
2010-10-17 23:33:26 +0000

    Added nextstate previousstate functions to gamestate class

--- trunk/include/GameState.h
+++ trunk/include/GameState.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#include "FSMState.h"
 #include "GameStd.h"
+#include "FSMState.h"
 
 class GameStateManager;
-class GameState;
 
 //Base class which all GameStates must inherit
 class GameState : public FSMState<GameState, IGameStateManager>{
 	friend class GameStateManager;
 public:
-	virtual void Pause() = 0;
-	virtual void Resume() = 0;	
-
 	virtual void Initialize()=0;
 	virtual void Update()=0;
 	virtual void Draw()=0;
 	virtual void Cleanup()=0;
+	virtual void Pause() = 0;
+	virtual void Resume() = 0;	
+	virtual void GotoPreviousState(){};
+	virtual void GotoNextState(){};
 };
