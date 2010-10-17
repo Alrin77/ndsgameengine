9869494ad2f08437f0e7d95d472293b6c78a3e0f
bmp410@gmail.com <bmp410@gmail.com@21144042-5242-e64c-d35b-eb64d47aa59c>
2010-10-17 23:17:18 +0000

    GameStateManager tested fixed another bug in FSM

--- source/GameStateIntro.cpp
+++ source/GameStateIntro.cpp
@@ -7,19 +7,12 @@ GameStateIntro::~GameStateIntro(){
 }
 
 void GameStateIntro::Initialize(){
-	for(int x = 0; x < 500; x++)
-	//***********************GET RID OF ME
-	pixel(rand() % 256, rand() % 192, RGB15(rand()%31, rand()%31, rand()%31));
-	//****************
 }
 
 void GameStateIntro::Update(){
 }
 
 void GameStateIntro::Draw(){
-	////***********************GET RID OF ME
-	//pixel(rand() % 256, rand() % 192, RGB15(rand()%31, rand()%31, rand()%31));
-	////***********************GET RID OF ME
 }
 
 void GameStateIntro::Cleanup(){
